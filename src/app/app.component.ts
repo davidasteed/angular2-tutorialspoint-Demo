6239e60c24b4f9ec626d3825d112d4dfd683ee1d
David Steed <dsteed@illuminatics.local>
2017-08-08 10:11:46 -0400

    now at the routes section of the tutorial...shows "not found" constantly for some reason

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,61 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component ({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <ul>
+    <li><a [routerLink] = "['/Product']">Product</a></li>
+    <li><a [routerLink] = "['/Inventory']">Inventory</a></li>
+  </ul>
+  <router-outlet></router-outlet>`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {}
+
+
+// import { Component } from '@angular/core';
+// import { IProduct } from './products';
+// import { ProductService } from './products.service';
+// import { appService } from './app.service';
+// import { Http, Response } from '@angular/http';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+
+// @Component({
+//   selector: 'my-app',
+//   template: '<div>Hello</div>',
+//   providers: [ProductService]
+
+  // templateUrl: 'app/app.component.html'
+
+  // example of an "inline template":
+  // template: `
+  // <h1>Hello {{name}}</h1>
+  // <h1>{{appTitle}}</h1>
+  // <div>To Tutorials Point</div>
+  // `,
+// })
+
+// export class AppComponent  {
+//   iproducts: IProduct[];
+//   constructor(private _product: ProductService) {}
+//   ngOnInit(): void {
+//     this._product.getproducts()
+//     .subscribe(iproducts => this.iproducts = iproducts);
+//   }
+
+  // name = 'World';
+  // appTitle: string = 'Welcome';
+  // appStatus: boolean = true;
+  // appList: any[] = [
+  //   {
+  //     'ID': '1',
+  //     'Name': 'One',
+  //     'url': 'app/Images/image1.jpg'
+  //   },
+  //   {
+  //     'ID': '2',
+  //     'Name': 'Two',
+  //     'url': 'app/Images/image2.jpg'
+  //   }
+  // ];
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,33 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { Appproduct } from './product.component';
+import { AppInventory } from './Inventory.component';
+import { PageNotFoundComponent } from './NotFound.component';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+const appRoutes: Routes = [
+  { path: 'Product', component: Appproduct },
+  { path: 'Inventory', component: AppInventory },
+  { path: '**', component: PageNotFoundComponent }
+];
 
-@NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+@NgModule ({
+  imports: [ BrowserModule,
+  RouterModule.forRoot(appRoutes)],
+  declarations: [ AppComponent, Appproduct, AppInventory, PageNotFoundComponent],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
+
+// import { NgModule }      from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppComponent }  from './app.component';
+// import { HttpModule } from '@angular/http';
+
+// @NgModule({
+//   imports:      [ BrowserModule, HttpModule ],
+//   declarations: [ AppComponent ],
+//   bootstrap:    [ AppComponent ]
+// })
+// export class AppModule {}
