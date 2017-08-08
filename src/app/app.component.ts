ad562821f729ade6b69b313e0f7527a21bc455a0
David Steed <dsteed@illuminatics.local>
2017-08-08 11:28:33 -0400

    completed tutorial up to the demo section, now on to angularCLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'my-app',
-  template: `
-  <ul>
-    <li><a [routerLink] = "['/Product']">Product</a></li>
-    <li><a [routerLink] = "['/Inventory']">Inventory</a></li>
-  </ul>
-  <router-outlet></router-outlet>`
+  template: '<product-form></product-form>'
 })
+
 export class AppComponent {}
+
+// @Component ({
+//   selector: 'my-app',
+//   template: `
+//   <ul>
+//     <li><a [routerLink] = "['/Product']">Product</a></li>
+//     <li><a [routerLink] = "['/Inventory']">Inventory</a></li>
+//   </ul>
+//   <router-outlet></router-outlet>`
+// })
+// export class AppComponent {}
 
 
 // import { Component } from '@angular/core';
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,12 @@
+export class Product {
+    constructor (
+        public productid: number,
+        public productname: string
+    ) {}
+}
+
 export interface IProduct {
     ProductID: number;
     ProductName: string;
 }
+
